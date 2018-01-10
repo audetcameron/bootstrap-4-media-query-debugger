fb84bf4f3f67df7f2588c58308beb933af78b27d
Cameron Audet <audetcameron@gmail.com>
2018-01-10 09:43:56 -0500

    Create jquery-bootstrap-4-media-query-debugger.js
    Using Jquery

---
+++ jquery-bootstrap-4-media-query-debugger.js
@@ -0,0 +1,43 @@
+    //using jQuery
+
+    function showViewPortSize(display) {
+    if(display) {
+      var jsHeight = window.innerHeight;
+      var jsWidth = window.innerWidth;
+      var mediaQ =null;
+
+    if (jsWidth > 320 && jsWidth <= 576) {
+      var mediaQ ='xs';
+    }else if (jsWidth > 577 && jsWidth <=768)  {
+      var mediaQ ='sm';
+    }else if (jsWidth > 769 && jsWidth <=992) {
+      var mediaQ = 'md';
+    }else if (jsWidth > 993 && jsWidth <= 1200) {
+      var mediaQ = 'lg';
+    }else if (jsWidth > 1200) {
+      var mediaQ = 'xl';
+    };
+      jQuery('body').prepend('<div id="viewportsize" style="font-size:11px;z-index:9999;position:fixed;top:40px;left:5px;color:#fff;background:#000;padding:10px">Height: '+jsHeight+'<br>Width: '+jsWidth+' <br> Media:  '+mediaQ+'</div>');
+      
+      jQuery(window).resize(function() {
+
+      var jsHeight = window.innerHeight;
+      var jsWidth = window.innerWidth;
+
+    if (jsWidth > 320 && jsWidth <= 576) {
+      var mediaQ ='xs';
+    }else if (jsWidth > 577 && jsWidth <=768)  {
+      var mediaQ ='sm';
+    }else if (jsWidth > 769 && jsWidth <=992) {
+      var mediaQ = 'md';
+    }else if (jsWidth > 993 && jsWidth <= 1200) {
+      var mediaQ = 'lg';
+    }else if (jsWidth > 1200) {
+      var mediaQ = 'xl';
+    };
+
+        jQuery('#viewportsize').html('Height: '+jsHeight+'<br>Width: '+jsWidth+' <br> Media: '+mediaQ);
+      });
+    }
+  }
+showViewPortSize(true);
