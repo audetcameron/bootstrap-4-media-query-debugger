bed30e491b33089818fad9b7d1f343607f669214
Cameron Audet <audetcameron@gmail.com>
2018-01-10 09:24:35 -0500

    Create bootstrap-4-media-query-debugger.js
    Initial Commit.

---
+++ bootstrap-4-media-query-debugger.js
@@ -0,0 +1,54 @@
+function showViewPortSize(display) {
+	var jsWidth = document.documentElement.clientWidth;
+	var jsHeight = document.documentElement.clientHeight;
+	var mediaQ =null;
+
+	if (jsWidth > 320 && jsWidth <= 576) {
+		var mediaQ ='xs';
+	}else if (jsWidth > 577 && jsWidth <=768)  {
+		var mediaQ ='sm';
+	}else if (jsWidth > 769 && jsWidth <=992) {
+		var mediaQ = 'md';
+	}else if (jsWidth > 993 && jsWidth <= 1200) {
+		var mediaQ = 'lg';
+	}else if (jsWidth > 1200) {
+		var mediaQ = 'xl';
+	};
+	//create the element
+	var viewportSize = document.createElement('div');
+	viewportSize.style.cssText = 'color:white;font-size:11px;z-index:9999;background:black;position:fixed;top:1px;left:5px;color:#fff;padding:10px;';
+	document.body.appendChild(viewportSize);
+	viewportSize.id = "viewportSize";
+	//add the innerHtml
+	viewportSize.innerHTML = 'Height: ' +jsHeight
+	+'<br>Width: ' +jsWidth
+	+'<br>Media: ' +mediaQ;
+
+	//on resize
+	window.addEventListener( 'resize', function( event ) {
+		var jsWidth = document.documentElement.clientWidth;
+		var jsHeight = document.documentElement.clientHeight;
+		var mediaQ =null;
+		if (jsWidth > 320 && jsWidth <= 576) {
+			var mediaQ ='xs';
+		}else if (jsWidth > 577 && jsWidth <=768)  {
+			var mediaQ ='sm';
+		}else if (jsWidth > 769 && jsWidth <=992) {
+			var mediaQ = 'md';
+		}else if (jsWidth > 993 && jsWidth <= 1200) {
+			var mediaQ = 'lg';
+		}else if (jsWidth > 1200) {
+			var mediaQ = 'xl';
+		};
+		//change innerHtml
+		viewportSize.innerHTML = 'Height: ' +jsHeight
+		+'<br>Width: ' +jsWidth
+		+'<br>Media: ' +mediaQ;
+
+	}, false ); //end resize
+
+}
+
+
+
+showViewPortSize(true);
