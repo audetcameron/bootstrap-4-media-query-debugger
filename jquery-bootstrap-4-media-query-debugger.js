9110ed5d34aaf91f5c75990cf50e2e85eb277314
Cameron Audet <audetcameron@gmail.com>
2019-09-11 14:40:16 -0400

    updated breakpoints

--- jquery-bootstrap-4-media-query-debugger.js
+++ jquery-bootstrap-4-media-query-debugger.js
@@ -6,15 +6,15 @@
       var jsWidth = window.innerWidth;
       var mediaQ =null;
 
-    if (jsWidth > 320 && jsWidth <= 576) {
+    if (jsWidth > 0 && jsWidth < 576) {
       var mediaQ ='xs';
-    }else if (jsWidth > 577 && jsWidth <=768)  {
+    }else if (jsWidth >= 576 && jsWidth < 768)  {
       var mediaQ ='sm';
-    }else if (jsWidth > 769 && jsWidth <=992) {
+    }else if (jsWidth >= 768 && jsWidth < 992) {
       var mediaQ = 'md';
-    }else if (jsWidth > 993 && jsWidth <= 1200) {
+    }else if (jsWidth >= 992 && jsWidth < 1200) {
       var mediaQ = 'lg';
-    }else if (jsWidth > 1200) {
+    }else if (jsWidth >= 1200) {
       var mediaQ = 'xl';
     };
       jQuery('body').prepend('<div id="viewportsize" style="font-size:11px;z-index:9999;position:fixed;top:40px;left:5px;color:#fff;background:#000;padding:10px">Height: '+jsHeight+'<br>Width: '+jsWidth+' <br> Media:  '+mediaQ+'</div>');
@@ -24,15 +24,15 @@
       var jsHeight = window.innerHeight;
       var jsWidth = window.innerWidth;
 
-    if (jsWidth > 320 && jsWidth <= 576) {
+    if (jsWidth > 0 && jsWidth < 576) {
       var mediaQ ='xs';
-    }else if (jsWidth > 577 && jsWidth <=768)  {
+    }else if (jsWidth >= 576 && jsWidth < 768)  {
       var mediaQ ='sm';
-    }else if (jsWidth > 769 && jsWidth <=992) {
+    }else if (jsWidth >= 768 && jsWidth < 992) {
       var mediaQ = 'md';
-    }else if (jsWidth > 993 && jsWidth <= 1200) {
+    }else if (jsWidth >= 992 && jsWidth < 1200) {
       var mediaQ = 'lg';
-    }else if (jsWidth > 1200) {
+    }else if (jsWidth >= 1200) {
       var mediaQ = 'xl';
     };
 
